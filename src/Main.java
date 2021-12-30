254bfc2049154ca0fb825ee6fa6ce791437fd8cb
Asude Şenözlüler <senozlulerasude@gmail.com>
2021-12-30 21:15:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        WomengameCalculator womengameCalculator=new WomengameCalculator();
+           womengameCalculator.hesapla();
+           womengameCalculator.gameOver();
+         GameCalculator gameCalculator=new WomengameCalculator();
+
+    }
+}
---
+++ src/WomengameCalculator.java
@@ -0,0 +1,5 @@
+public class WomengameCalculator extends GameCalculator{
+    public void hesapla(){
+        System.out.println("Puanınız: 95");
+    }
+}
